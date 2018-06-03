b13e049504321bfd3aea67694f23bb22bc23c74b
Ian Wright <idwright90@gmail.com>
2018-06-03 13:36:27 -0400

    Apply StandardJS fix

--- public/javascripts/conway.js
+++ public/javascripts/conway.js
@@ -12,13 +12,9 @@ var height = canvas.height
 var hCells = Math.floor(width / CELL_SIZE)
 var vCells = Math.floor(height / CELL_SIZE)
 
-var gameWidth = hCells * CELL_SIZE
-var gameHeight = vCells * CELL_SIZE
-
 var x, y
 
-function game () {
-
+function game () { // eslint-disable-line no-unused-vars
   var i, j, initStatus
   var initGen = []
 
@@ -29,7 +25,7 @@ function game () {
   for (i = 0; i < vCells; i++) {
     for (j = 0; j < hCells; j++) {
       initStatus = Math.floor(Math.random() * 10)
-      
+
       if (initStatus < 5) {
         if (!initGen[i]) { initGen[i] = [] }
         initGen[i][j] = true
@@ -55,9 +51,7 @@ function update (currentGen) {
       fieldCount = getLivingNeighbors(currentGen, i, j)
       if (currentGen[i][j]) { fieldCount++ }
 
-      if (fieldCount === 3) { nextGen[i][j] = true }
-      else if (fieldCount === 4) { } // do nothing
-      else { nextGen[i][j] = false }
+      if (fieldCount === 3) { nextGen[i][j] = true } else if (fieldCount === 4) { } else { nextGen[i][j] = false }
     }
   }
   draw(nextGen)
@@ -68,10 +62,10 @@ function draw (gen) {
   x = 0
   y = 0
 
+  var i, j
   for (i = 0; i < vCells; i++) {
     for (j = 0; j < hCells; j++) {
-      if(gen[i][j]) { context.fillStyle = 'black' }
-      else { context.fillStyle = 'white' }
+      if (gen[i][j]) { context.fillStyle = 'black' } else { context.fillStyle = 'white' }
 
       context.fillRect(x, y, CELL_SIZE, CELL_SIZE)
       x += CELL_SIZE
@@ -82,17 +76,17 @@ function draw (gen) {
   setTimeout(function () { update(gen) }, 50)
 }
 
-function getLivingNeighbors(arr, i, j) {
+function getLivingNeighbors (arr, i, j) {
   var count = 0
 
-  if (i - 1 >= 0 && j - 1 >= 0 && arr[i - 1][j - 1])         { count++ }
-  if (i - 1 >= 0 && arr[i - 1][j])                           { count++ }
-  if (i - 1 >=0 && j + 1 < hCells && arr[i - 1][j + 1])      { count++ }
-  if (j + 1 < hCells && arr[i][j + 1])                       { count++ }
+  if (i - 1 >= 0 && j - 1 >= 0 && arr[i - 1][j - 1]) { count++ }
+  if (i - 1 >= 0 && arr[i - 1][j]) { count++ }
+  if (i - 1 >= 0 && j + 1 < hCells && arr[i - 1][j + 1]) { count++ }
+  if (j + 1 < hCells && arr[i][j + 1]) { count++ }
   if (i + 1 < vCells && j + 1 < hCells && arr[i + 1][j + 1]) { count++ }
-  if (i + 1 < vCells && arr[i + 1][j])                       { count++ }
-  if (i + 1 < vCells && j - 1 >= 0 && arr[i + 1][j - 1])     { count++ }
-  if (j - 1 >= 0 && arr[i][j - 1])                           { count++ }
+  if (i + 1 < vCells && arr[i + 1][j]) { count++ }
+  if (i + 1 < vCells && j - 1 >= 0 && arr[i + 1][j - 1]) { count++ }
+  if (j - 1 >= 0 && arr[i][j - 1]) { count++ }
 
   return count
 }
--- routes/fun.js
+++ routes/fun.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.render('fun');
-});
+  res.render('fun')
+})
 
 router.get('/smb2', function (req, res, next) {
-  res.render('fun/smb2');
-});
+  res.render('fun/smb2')
+})
 
 router.get('/conway', function (req, res, next) {
-  res.render('fun/conway');
-});
+  res.render('fun/conway')
+})
 
-module.exports = router;
+module.exports = router
